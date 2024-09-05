becf696746d9009a1bb25cae600c0da2be4df5af
kimdonghwa213 <100815876+kimdonghwa213@users.noreply.github.com>
2024-09-05 12:41:19 +0900

    Create temp_copy.py

---
+++ hw02/temp_copy.py
@@ -0,0 +1,13 @@
+def f2c(temp_f: float) -> float:
+    return (temp_f - 32) * 5 / 9
+
+def main():
+    temp_f = 75
+   #temp_c = (temp_f - 32) * 5 / 9
+    temp_c = f2c(temp_f)
+    print(f"{temp_f}F => {temp_c:.1f}C")
+
+    pass
+
+if __name__ == '__main__':
+    main()
