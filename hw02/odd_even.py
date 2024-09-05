d8c1d44f0896ed673ae3be945b5f38aec5dd93b3
kimdonghwa213 <100815876+kimdonghwa213@users.noreply.github.com>
2024-09-05 12:35:11 +0900

    Create odd_even.py

---
+++ hw02/odd_even.py
@@ -0,0 +1,13 @@
+def is_even(n):
+    if n % 2 == 0:
+        return n
+
+def main():
+    n = 4
+    if n % 2 == 0:
+        print(f"{n}은 짝수입니다.")
+    else:
+        print(f"{n}은 홀수입니다.")
+
+if __name__ == '__main__':
+    main()
