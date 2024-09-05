b7e7d94f89aa3f6525354ce797b05906b5690eb1
kimdonghwa213 <100815876+kimdonghwa213@users.noreply.github.com>
2024-09-05 12:45:58 +0900

    Create sum100.py

---
+++ hw02/sum100.py
@@ -0,0 +1,17 @@
+from lec01.odd_even import is_even
+
+
+def main():
+    total = 0
+
+    for i in range(1, 101):
+         #total = total + i
+        if is_even(i):
+            total +=i
+    print(f"1부터 100까지 짝수의 합은 {total}입니다.")
+
+    even_100 = [i for i in range(1, 101) if i % 2 == 0]
+
+
+if __name__ == '__main__':
+    main()
