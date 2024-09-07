093218017835163e28519e84647734cb6bfd064e
kimdonghwa213 <100815876+kimdonghwa213@users.noreply.github.com>
2024-09-08 00:26:58 +0900

    Update sum100.py

--- hw02/sum100.py
+++ hw02/sum100.py
@@ -5,13 +5,12 @@ def main():
     total = 0
 
     for i in range(1, 101):
-         #total = total + i
         if is_even(i):
             total +=i
     print(f"1부터 100까지 짝수의 합은 {total}입니다.")
 
     even_100 = [i for i in range(1, 101) if i % 2 == 0]
-
+    print(even_100)
 
 if __name__ == '__main__':
     main()
