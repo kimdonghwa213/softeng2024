831b2152236e09f6973d7569ad5ee986bad66dd7
kimdonghwa213 <100815876+kimdonghwa213@users.noreply.github.com>
2024-09-10 11:57:46 +0900

    Update odd_even_gui.py

--- hw03/odd_even_gui.py
+++ hw03/odd_even_gui.py
@@ -2,6 +2,10 @@ import tkinter as tk
 from tkinter import simpledialog
 from lec01.odd_even import is_even
 
+def is_odd(n):
+    if n % 2 == 1:
+        return n
+
 ROOT = tk.Tk()
 
 ROOT.withdraw()
@@ -10,5 +14,7 @@ USER_INP = int(simpledialog.askstring("Odd Even Test", "Enter number of odd even
 
 if is_even(USER_INP):
     print(f"{USER_INP} is an even number")
-else:
+elif is_odd(USER_INP):
     print(f"{USER_INP} is an odd number")
+else:
+    print(f"{USER_INP} is 0")
