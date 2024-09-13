0a960ffc23dfa453cf8ce8284e4e17b676b9f717
kimdonghwa213 <100815876+kimdonghwa213@users.noreply.github.com>
2024-09-13 13:24:41 +0900

    Update prime_number_gui.py

--- hw03/prime_number_gui.py
+++ hw03/prime_number_gui.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 from tkinter import simpledialog
 from lec01.prime_number import is_prime
+
 ROOT = tk.Tk()
 
 ROOT.withdraw()
 
-USER_INT =int(simpledialog.askinteger("User","Enter a number:"))
+USER_INT =int(simpledialog.askinteger(title="소수 구하기",prompt="숫자를 입력하시오."))
 
 if is_prime(USER_INT):
-    print(f"{USER_INT} is a prime number")
+    print(f"{USER_INT} 은(는) 소수입니다.")
 else:
-    print(f"{USER_INT} is not a prime number")
+    print(f"{USER_INT} 은(는) 소수가 아닙니다.")
