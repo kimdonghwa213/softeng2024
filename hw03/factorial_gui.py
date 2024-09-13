e4c3ab00e390ce39a382fbf9208dfc231993a4f8
kimdonghwa213 <100815876+kimdonghwa213@users.noreply.github.com>
2024-09-13 13:33:35 +0900

    Update factorial_gui.py

--- hw03/factorial_gui.py
+++ hw03/factorial_gui.py
@@ -6,6 +6,6 @@ ROOT = tk.Tk()
 
 ROOT.withdraw()
 
-USER_INT =int(simpledialog.askinteger("팩토리얼 구하기","숫자를 입력하시오."))
+USER_INT =int(simpledialog.askinteger(title="팩토리얼 구하기",prompt="숫자를 입력하시오."))
 
 print(f"{USER_INT}! = {factorial(USER_INT)} 입니다.")
