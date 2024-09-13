68d02f9e7567060fb7dcb2fc4bca1efb914cc5b3
kimdonghwa213 <100815876+kimdonghwa213@users.noreply.github.com>
2024-09-13 13:19:30 +0900

    Update odd_even_gui.py

--- hw03/odd_even_gui.py
+++ hw03/odd_even_gui.py
@@ -2,10 +2,6 @@ import tkinter as tk
 from tkinter import simpledialog
 from lec01.odd_even import is_even
 
-def is_odd(n):
-    if n % 2 == 1:
-        return n
-
 ROOT = tk.Tk()
 
 ROOT.withdraw()
@@ -14,7 +10,5 @@ USER_INP = int(simpledialog.askstring("홀짝테스트", "숫자를 입력하시
 
 if is_even(USER_INP):
     print(f"{USER_INP} 은(는) 짝수입니다.")
-elif is_odd(USER_INP):
-    print(f"{USER_INP} 은(는) 홀수입니다.")
 else:
-    print(f"{USER_INP} is 0")
+    print(f"{USER_INP} 은(는) 홀수입니다.")
