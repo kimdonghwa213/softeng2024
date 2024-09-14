c1e56614e45aeadebdedb48815600bd191f25c3e
kimdonghwa213 <100815876+kimdonghwa213@users.noreply.github.com>
2024-09-15 00:22:48 +0900

    Update sum100_gui.py

--- hw03/sum100_gui.py
+++ hw03/sum100_gui.py
@@ -6,14 +6,14 @@ ROOT = tk.Tk()
 
 ROOT.withdraw()
 
-USER_INT =int(simpledialog.askinteger(title="짝수의 합 구하기",prompt="숫자를 입력하시오"))
+USER_INP =int(simpledialog.askinteger(title="짝수의 합 구하기",prompt="숫자를 입력하시오"))
 
 total = 0
 
-for i in range(1, USER_INT+1):
+for i in range(1, USER_INP+1):
     if is_even(i):
         total += i
-print(f"1부터 {USER_INT}까지 짝수의 합은 {total}입니다.")
+print(f"1부터 {USER_INP}까지 짝수의 합은 {total}입니다.")
 
-even_sum = [i for i in range(1, USER_INT+1) if i % 2 == 0]
+even_sum = [i for i in range(1, USER_INP+1) if i % 2 == 0]
 print(even_sum)
