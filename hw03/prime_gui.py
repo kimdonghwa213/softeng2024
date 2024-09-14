b2082bf623b5b0e23833b64794453bd2667e5342
kimdonghwa213 <100815876+kimdonghwa213@users.noreply.github.com>
2024-09-15 00:21:49 +0900

    Update prime_gui.py

--- hw03/prime_gui.py
+++ hw03/prime_gui.py
@@ -6,11 +6,11 @@ ROOT = tk.Tk()
 
 ROOT.withdraw()
 
-USER_INT =int(simpledialog.askinteger(title="범위의 소수 구하기",prompt="숫자를 입력하시오."))
+USER_INP =int(simpledialog.askinteger(title="범위의 소수 구하기",prompt="숫자를 입력하시오."))
 
 primes = []
-for i in range(2,USER_INT):
+for i in range(2,USER_INP):
     if is_prime(i):
         primes.append(i)
 
-print(f"1부터 {USER_INT}까지의 수 중 소수는 {primes}입니다.")
+print(f"1부터 {USER_INP}까지의 수 중 소수는 {primes}입니다.")
