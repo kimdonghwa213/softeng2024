3d92b07947b447c37d2d31394473ddfff7349604
kimdonghwa213 <100815876+kimdonghwa213@users.noreply.github.com>
2024-09-15 00:22:22 +0900

    Update prime_number_gui.py

--- hw03/prime_number_gui.py
+++ hw03/prime_number_gui.py
@@ -6,9 +6,9 @@ ROOT = tk.Tk()
 
 ROOT.withdraw()
 
-USER_INT =int(simpledialog.askinteger(title="소수 구하기",prompt="숫자를 입력하시오."))
+USER_INP =int(simpledialog.askinteger(title="소수 구하기",prompt="숫자를 입력하시오."))
 
-if is_prime(USER_INT):
-    print(f"{USER_INT} 은(는) 소수입니다.")
+if is_prime(USER_INP):
+    print(f"{USER_INP} 은(는) 소수입니다.")
 else:
-    print(f"{USER_INT} 은(는) 소수가 아닙니다.")
+    print(f"{USER_INP} 은(는) 소수가 아닙니다.")
