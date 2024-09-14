5332a7c381776f508c7c69832863c1d9ee8bfbf1
kimdonghwa213 <100815876+kimdonghwa213@users.noreply.github.com>
2024-09-15 00:23:10 +0900

    Update temp_copy_gui.py

--- hw03/temp_copy_gui.py
+++ hw03/temp_copy_gui.py
@@ -6,8 +6,8 @@ ROOT = tk.Tk()
 
 ROOT.withdraw()
 
-USER_INT =int(simpledialog.askinteger(title="온도단위 변환기",prompt="변환시킬 화씨 온도를 입력하시오"))
+USER_INP =int(simpledialog.askinteger(title="온도단위 변환기",prompt="변환시킬 화씨 온도를 입력하시오"))
 
-temp_c = f2c(USER_INT)
+temp_c = f2c(USER_INP)
 
-print(f"{USER_INT}F => {temp_c:.1f}C")
+print(f"{USER_INP}F => {temp_c:.1f}C")
