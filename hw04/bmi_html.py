e7c3b8585f6eb983e623df6e69e4db57f741c464
kimdonghwa213 <100815876+kimdonghwa213@users.noreply.github.com>
2024-09-21 23:40:32 +0900

    Update bmi_html.py

--- hw04/bmi_html.py
+++ hw04/bmi_html.py
@@ -3,22 +3,22 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def bmi():
     html_str = """
 
     <!DOCTYPE html>
  <html lang="kr">
  <head>
  <meta charset="UTF-8">
- <title>Flask Home Page</title>
+ <title>BMI 계산기</title>
  </head>
  <body>
  <form method="GET" action="/bmi">
- <h2>bmi 계산기</h2>
- <label>키(height) :
- <input type="text" name="height"> </n> 
- 몸무게(weight) :
- <input type="text" name="weight"> </n>
+ <h2> BMI 계산기</h2>
+ <label>키(cm) :
+ <input type="text" name="height"> 
+ 몸무게(kg) :
+ <input type="text" name="weight">
  </label>
  <button type="submit">출력</button>
  </form>
@@ -32,8 +32,7 @@ def bmi_html():
     height = float(request.args.get("height"))
     weight = float(request.args.get("weight"))
     bmi = weight / ((height / 100) ** 2)
-    html_str = f"당신의 bmi는 {bmi:.2f}입니다.</strong><br>"
+    html_str = f"당신의 BMI는 {bmi:.2f}입니다.</strong><br>"
     return html_str
-
 
 app.run(debug=True)
