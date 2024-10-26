7e87b33a013bd07aeb6e44a19c9db04a810a7dab
kimdonghwa213 <100815876+kimdonghwa213@users.noreply.github.com>
2024-10-26 23:28:12 +0900

    Update views.py

--- hw09/single_pages/views.py
+++ hw09/single_pages/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 import pandas as pd
 
-
 # Create your views here.
 def landing(request):
-    return render(request, 'single_pages/index.html')
+    return render(request,'single_pages/index.html', {'title':'Home'})
 
 def about_me(request):
-    return render(request, 'single_pages/about_me.html')
+    return render(request,'single_pages/about_me.html', {'title': 'About Me'})
 
 def blog(request):
     df = pd.read_csv('./blog.csv')
@@ -18,8 +17,7 @@ def blog(request):
             "content": row["content"]
         })
     print(post_list)
-    return render(request, 'single_pages/blog.html', context={'title': 'blog post', 'posts': post_list})
+    return render(request, 'single_pages/blog.html', {'title' : 'Blog', 'posts': post_list})
 
 def career(request):
-    return render(request, 'single_pages/career.html')
-
+    return render(request, 'single_pages/career.html', {'title': 'Career'})
