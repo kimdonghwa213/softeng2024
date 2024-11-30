3b640ba185ca5ebf01ee1e55f8158eab55c7b48c
kimdonghwa213 <100815876+kimdonghwa213@users.noreply.github.com>
2024-11-30 16:16:54 +0900

    Add files via upload

---
+++ hw13/blog/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post, Category
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
+class PostDetail(DetailView):
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#     return render(request, 'blog/index.html',{'posts':posts,})
+
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+
+#     return render(request, 'blog/single_post.html', {'post':post})
