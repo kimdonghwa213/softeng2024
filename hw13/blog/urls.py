5fbc0b8690761fae69dfbc82cae73b63bfbac31e
kimdonghwa213 <100815876+kimdonghwa213@users.noreply.github.com>
2024-12-01 13:45:17 +0900

    Add files via upload

---
+++ hw13/blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('tag/<str:slug>/', views.tag_page),
+    path('category/<str:slug>/', views.category_page),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view(), name='post_list'),
+    # path('<int:pk>/', views.single_post_page),
+    # path('', views.index),
+]
